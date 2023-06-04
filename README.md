5b66657f189b202f5d56d84b3d18f5d6efefe5e0
Eyelly Wu <eyelly.wu@gmail.com>
2023-06-04 22:13:17 +0800

    docs: update the link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 # Summary
 This is an online application example of the  `@i18n-pro/react`  library, which can be accessed and previewed through the following two online editing platforms
-* CodeSandBox
-* StackBlitz
+* [Open in CodeSandbox](https://codesandbox.io/p/github/i18n-pro/react-demo/main?file=README.md)
+* [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz_small.svg "Open in StackBlitz")](https://stackblitz.com/github/i18n-pro/react-demo?file=README.md)
 
--- docs/index.tsx
+++ docs/index.tsx
@@ -1,15 +1,26 @@
-import { Bold, Break, CodeBlock, H1, Image, List, render } from 'jsx-to-md'
+import { H1, Image, Link, List } from 'jsx-to-md'
 import { initI18n } from 'i18n-pro'
 import en from './i18n/en.json'
 
-export default function MarkDown(props: any) {
+const localeSuffixMap = {
+  zh: 'zh-CN',
+  en: '',
+}
+
+const repoName = 'i18n-pro/react-demo'
+
+export default function MarkDown(props: { locale: 'en' | 'zh' }) {
   const { t } = initI18n({
-    namespace: props.locale,
+    namespace: props.locale + '',
     locale: props.locale,
     langs: {
       en,
     },
   })
+
+  const suffix = localeSuffixMap[props.locale]
+
+  const filename = `README${suffix ? `_${suffix}` : ''}.md`
 
   return (
     <>
@@ -18,7 +29,25 @@ export default function MarkDown(props: any) {
         '这是{0}库的在线应用示例，可通过如下两种在线编辑平台访问预览',
         ' `@i18n-pro/react` ',
       )}
-      <List items={['U', 'CodeSandBox', 'StackBlitz']} />
+      <List
+        items={[
+          'U',
+          <Link
+            href={`https://codesandbox.io/p/github/${repoName}/main?file=${filename}`}
+          >
+            Open in CodeSandbox
+          </Link>,
+          <Link
+            href={`https://stackblitz.com/github/${repoName}?file=${filename}`}
+          >
+            <Image
+              alt="Open in StackBlitz"
+              title="Open in StackBlitz"
+              src="https://developer.stackblitz.com/img/open_in_stackblitz_small.svg"
+            />
+          </Link>,
+        ]}
+      />
     </>
   )
 }
